84b3fd811861d12bc6198e521b788132c18eea3d
Aaron Dalton <aaron@daltons.ca>
2021-10-05 13:18:44 -0600

    Commit of basic skeleton

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'APGames.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'APGames',
+    libraryTarget: 'var'
+  }
+};

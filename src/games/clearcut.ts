0eb2518235aaae1eb00d961bd1a0d0599e1c0bf0
Aaron Dalton <aaron@daltons.ca>
2023-07-26 11:23:58 -0600

    Clearcut: Going live

--- src/games/clearcut.ts
+++ src/games/clearcut.ts
@@ -70,7 +70,7 @@ export class ClearcutGame extends GameBase {
                 urls: ["http://www.marksteeregames.com/"],
             }
         ],
-        flags: ["experimental", "pie", "automove"]
+        flags: ["pie", "automove"]
     };
     public static coords2algebraic(x: number, y: number): string {
         return GameBase.coords2algebraic(x, y, 19);

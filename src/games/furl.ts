ca335c52e1621f8d073b47432afd0da5b7d4e97a
Aaron Dalton <aaron@daltons.ca>
2023-12-29 14:55:00 -0700

    Furl: Add 'perspective' flag

--- src/games/furl.ts
+++ src/games/furl.ts
@@ -43,7 +43,7 @@ export class FurlGame extends GameBase {
                 urls: ["http://www.mrraow.com"]
             }
         ],
-        flags: ["experimental", "multistep", "check"],
+        flags: ["experimental", "multistep", "check", "perspective"],
         variants: [
             // { uid: "size-5", group: "board" },
         ],

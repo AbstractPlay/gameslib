c8c653797a52fff4ed524b62d49478f5261f30c4
paul <yewhonwa@gmail.com>
2024-01-02 23:54:05 +0800

    Add a hex slanted graph.

--- src/common/graphs/index.ts
+++ src/common/graphs/index.ts
@@ -4,8 +4,9 @@ import { SquareDiagGraph } from "./square-diag";
 import { SquareFanoronaGraph } from "./square-fanorona";
 import { SnubSquareGraph } from "./snubsquare";
 import { HexTriGraph } from "./hextri";
+import { HexSlantedGraph } from "./hex-slanted";
 import { BaoGraph } from "./bao";
 import { SowingNoEndsGraph } from "./sowing-no-ends";
 import { IGraph } from "./IGraph";
 
-export { IGraph, SquareGraph, SquareOrthGraph, SquareDiagGraph, SquareFanoronaGraph, SnubSquareGraph, HexTriGraph, BaoGraph, SowingNoEndsGraph };
+export { IGraph, SquareGraph, SquareOrthGraph, SquareDiagGraph, SquareFanoronaGraph, SnubSquareGraph, HexTriGraph, HexSlantedGraph, BaoGraph, SowingNoEndsGraph };

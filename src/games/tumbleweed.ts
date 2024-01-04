43a6a2de251558abfb7a44f5f76919fb1c432835
Aaron Dalton <aaron@daltons.ca>
2024-01-04 06:22:23 -0700

    Tumbleweed: Minor tweaks

--- src/games/tumbleweed.ts
+++ src/games/tumbleweed.ts
@@ -40,7 +40,7 @@ export class TumbleweedGame extends GameBase {
         people: [
             {
                 type: "designer",
-                name: "Michał Zapała",
+                name: "Mike Zapawa",
             }
         ],
         flags: ["pie", "multistep", "scores"],
@@ -600,7 +600,7 @@ export class TumbleweedGame extends GameBase {
 
         let points1: {row: number, col: number}[] = [];
         let points2: {row: number, col: number}[] = [];
-        if (showInfluence) {
+        if (showInfluence || this.gameover) {
             const points = this.influenceMarkers();
             points1 = points.get(1)!;
             points2 = points.get(2)!;

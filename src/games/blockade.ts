951ff86da3b2c3664f173c44a4a02c0d886d6b5e
paul <yewhonwa@gmail.com>
2024-03-08 00:11:35 +0800

    Revert "Blockade: Handle empty string because the handleClicks sometimes generates empty strings."
    This reverts commit ff705915a5be0a76e88ee79511e37fcd5f46aa68.

--- src/games/blockade.ts
+++ src/games/blockade.ts
@@ -835,7 +835,7 @@ export class BlockadeGame extends GameBase {
             if (!result.valid) {
                 throw new UserFacingError("VALIDATION_GENERAL", result.message);
             }
-            if (m !== "" && !partial && !this.moves().includes(m)) {
+            if (!partial && !this.moves().includes(m)) {
                 throw new UserFacingError("VALIDATION_FAILSAFE", i18next.t("apgames:validation._general.FAILSAFE", {move: m}));
             }
         }

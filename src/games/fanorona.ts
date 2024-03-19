3f792d0661454621f555b0abebbbf61cddf7fa9c
Aaron Dalton <aaron@daltons.ca>
2024-03-18 20:40:41 -0600

    Toguz & Fanorona: Add traditional tag

--- src/games/fanorona.ts
+++ src/games/fanorona.ts
@@ -36,7 +36,7 @@ export class FanoronaGame extends GameBase {
         urls: [
             "https://en.wikipedia.org/wiki/Fanorona",
         ],
-        categories: ["goal>annihilate", "mechanic>capture", "mechanic>move", "board>shape>rect", "board>connect>other", "components>simple"],
+        categories: ["goal>annihilate", "mechanic>capture", "mechanic>move", "mechanic>traditional", "board>shape>rect", "board>connect>other", "components>simple"],
         flags: ["perspective", "limited-pieces", "multistep", "no-moves", "aiai"]
     };
 

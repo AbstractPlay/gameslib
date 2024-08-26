a6906a4f9d2748d4a0346e47a30e17c5eb4bd435
Noé Falzon <1331434+Castux@users.noreply.github.com>
2024-08-26 11:14:32 +0300

    Tritium: grey central hexagon

--- src/games/tritium.ts
+++ src/games/tritium.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { GameBase, IAPGameState, IClickResult, IIndividualState, IValidationResult, IScores } from "./_base";
 import { APGamesInformation } from "../schemas/gameinfo";
-import { APRenderRep } from "@abstractplay/renderer/src/schemas/schema";
+import { APRenderRep, RowCol } from "@abstractplay/renderer/src/schemas/schema";
 import { APMoveResult } from "../schemas/moveresults";
 import { HexTriGraph, reviver, UserFacingError } from "../common";
 import { Glyph } from "@abstractplay/renderer";
@@ -472,6 +472,13 @@ export class TritiumGame extends GameBase {
             }
         }
 
+        // Central hexagon
+
+        const center: [RowCol, ...RowCol[]] = [{ row: this.boardsize - 1, col: this.boardsize - 1}];
+        const markers: Array<any> | undefined = [
+            { type: "flood", colour: "#888", opacity: 0.25, points: center}
+        ];
+
         // Build rep
 
         const rep: APRenderRep =  {
@@ -480,8 +487,9 @@ export class TritiumGame extends GameBase {
                 style: "hex-of-hex",
                 minWidth: this.boardsize,
                 maxWidth: this.boardsize * 2 - 1,
-                blocked: [{row: this.boardsize - 1, col: this.boardsize - 1}],
-                alternatingSymmetry: false
+                blocked: center,
+                alternatingSymmetry: false,
+                markers
             },
             legend: {
                 T1: [

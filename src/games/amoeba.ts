a4c719ef7e074ac6e5b01559ea5c7a2188ca8364
Aaron Dalton <aaron@daltons.ca>
2025-01-12 15:37:04 -0700

    Amoeba: Oops. Forgot the 'else'. Really fixed now.

--- src/games/amoeba.ts
+++ src/games/amoeba.ts
@@ -160,8 +160,9 @@ export class AmoebaGame extends GameBase {
                             const ray = g.ray(fx, fy, bearing).map(c => g.coords2algebraic(...c));
                             if (ray.length - 1 < fstack.length) {
                                 newmove = cell;
+                            } else {
+                                newmove = `${left}>${ray[fstack.length - 1]}`;
                             }
-                            newmove = `${left}>${ray[fstack.length - 1]}`;
                         }
                     }
                     // direct move

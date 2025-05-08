ccd0c2941b0c5f43449e438d4e5610b3309063c8
Aaron Dalton <aaron@daltons.ca>
2025-05-08 11:49:03 -0600

    Oops! Forgot to git add the eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ["src/schemas/*", "src/ais/*", "playground/*", "bin/*", "build/*", "dist/*", "docs/*", "src/games/tafl/ruleset.d.ts", "**/*.config.js"],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    rules: {
+        "@typescript-eslint/no-explicit-any": "warn",
+    },
+  }
+);
+
+

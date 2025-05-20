4ee1208d9bf7b5a374aa7428d2d3551dbd216b7b
Aaron Dalton <aaron@daltons.ca>
2025-05-20 10:38:35 -0600

    Quincunx: Try skipping automoves by using emulation

--- src/games/quincunx.ts
+++ src/games/quincunx.ts
@@ -488,7 +488,7 @@ export class QuincunxGame extends GameBase {
         return {basics, draws, pairs, straights, sets, flushes, powerplay, powerplayScore};
     }
 
-    public move(m: string, {trusted = false, partial = false} = {}): QuincunxGame {
+    public move(m: string, {trusted = false, partial = false, emulation = false} = {}): QuincunxGame {
         if (this.gameover) {
             throw new UserFacingError("MOVES_GAMEOVER", i18next.t("apgames:MOVES_GAMEOVER"));
         }
@@ -515,7 +515,7 @@ export class QuincunxGame extends GameBase {
             }
         }
 
-        if (partial) { return this; }
+        if (partial || emulation) { return this; }
         this.results = [];
 
         let lastmove = m;

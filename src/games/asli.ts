93a74720532cb2af0ead32b750d589c26f203f56
Aaron Dalton <aaron@daltons.ca>
2025-06-14 12:15:52 -0600

    Revert "Asli: Colour fix"
    This reverts commit b3d2824a9c0e47cc7fdfa7cb84aaa473b65b29ed.

--- src/games/asli.ts
+++ src/games/asli.ts
@@ -201,14 +201,14 @@ export class AsliGame extends GameBase {
     }
 
     public getPlayerColour(p: playerid): number|string {
-        if (!this.variants.includes("setkomi")) {
-            if (this.stack.length === 1) {
-                return "#999";
-            } else {
-                return this.stack[1].lastmove === "pie" ? p : p === 1 ? 2 : 1;
-            }
+        if (this.stack.length ===  1) {
+            return "#999";
         } else {
-            return p;
+            if (!this.variants.includes("setkomi")) {
+                return this.stack[1].lastmove === "pie" ? p : p === 1 ? 2 : 1;
+            } else {
+                return p;
+            }
         }
     }
 

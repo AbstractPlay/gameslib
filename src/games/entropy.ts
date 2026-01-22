816439bf0aa001547b559489f11e65de6c8ffcc3
Aaron Dalton <aaron@daltons.ca>
2026-01-22 11:51:38 -0700

    Entropy: No reason not to have 6x6

--- src/games/entropy.ts
+++ src/games/entropy.ts
@@ -57,7 +57,7 @@ export class EntropyGame extends GameBaseSimultaneous {
             },
         ],
         variants: [
-            // {uid: "6x6", group: "board"},
+            {uid: "6x6", group: "board"},
             {uid: "5x5", group: "board"},
         ],
         displays: [{uid: "piece-numbers"}],

e2987b09f0cad9c12a3e9bda5a14413b5ab18ceb
João Pedro Neto <886455+jpneto@users.noreply.github.com>
2026-06-15 14:28:13 +0100

    Corrected Kanare's apid

--- src/games/stairs.ts
+++ src/games/stairs.ts
@@ -40,7 +40,7 @@ export class StairsGame extends GameBase {
                 type: "designer",
                 name: "Kanare Kato",
                 urls: ["https://kanare-abstract.com/en"],
-                apid: "94d8e4a8-d091-7018-b8e4-7fcb34ae7d6c",
+                apid: "0998417b-d2b5-4a3f-8f5d-965e67b290b8",
             },
             {
                 type: "coder",
